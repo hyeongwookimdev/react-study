b8cd40c80c85de62f636c455264f1621510e92be
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-09-27 15:29:18 +0900

    fix(route): route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Detail from "./routes/Detail";
 
 const router = createBrowserRouter([
   {
-    path: `/`,
+    path: `${process.env.PUBLIC_URL}/`,
     element: <Home />,
   },
   {
-    path: `/movie/:id`,
+    path: `${process.env.PUBLIC_URL}/movie/:id`,
     element: <Detail />,
   },
 ]);
