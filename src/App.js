e0a2dfba6b96ea3b61fbd22877d73a91cb6eedc3
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-09-27 10:36:12 +0900

    Create repository

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Detail />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
